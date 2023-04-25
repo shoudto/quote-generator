0d0cd8384a97393bc27952d31db780ec4c1c4aa1
Oscar Shoudt <oscarshoudt@Oscars-MBP.home>
2023-04-25 19:06:59 -0400

    added loader style

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading(){
+    // console.log(loader)
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote(){
+    loading();
+
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -25,11 +41,16 @@ function newQuote(){
     } else {
         quoteText.classList.remove('long-quote');
     } 
+
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // GET quotes From API
 async function getQuotes(){
+    loading();
+
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
